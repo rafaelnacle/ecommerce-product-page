653f2baa2f6b2315f7fb5020d490c65fb2615a37
Rafael Nacle <rafaelnacle@hotmail.com>
2022-07-07 16:32:48 -0300

    Image modal, mobile responsive layout

---
+++ assets/scripts/imagePreview.js
@@ -0,0 +1,40 @@
+"use-strict"
+
+// Open the Modal
+function openModal() {
+  document.getElementById("modal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("thumbnail");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  // dots[slideIndex - 1].className += " active";
+}
--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -67,5 +67,3 @@ deleteItemFromCart.addEventListener('click', () => {
 })
 
 
-
-
