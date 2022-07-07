74470d3c2a61863daecb800e5aebebf87c871773
Rafael Nacle <rafaelnacle@hotmail.com>
2022-07-07 14:38:14 -0300

    Cart add and remove item, item total

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -5,8 +5,22 @@ const minusButton = document.getElementById("minus-button");
 const itemQuantity = document.getElementById("item-quantity");
 const addToCartButton = document.getElementById('add-to-cart-button');
 const cartItemQuantity = document.getElementById('cart-item-quantity');
-const cartItemValue = document.getElementById('cart-item-quantity');
-const cart = document.getElementById('cart');
+const cartItemValue = document.getElementById('cart-item-value');
+
+const cardLi = document.querySelector('.cart-li');
+const cartIcon = document.getElementById('cart');
+const cartCard = document.querySelector('.cart-card');
+const cartCardDiv = document.querySelector('.cart-card-div');
+
+const emptyCartDiv = document.querySelector('.empty-cart')
+
+const cartItem = document.querySelector('#cart-item');
+const cartTotal = document.querySelector('#cart-total');
+const itemPrice = document.querySelector('#item-price')
+
+const checkoutButton = document.querySelector('.checkout-button')
+
+const deleteItemFromCart = document.querySelector('.trash-icon')
 
 let quantity = 0;
 
@@ -22,8 +36,13 @@ plusButton.addEventListener('click', () => {
   itemQuantity.innerHTML = quantity;
 })
 
+const cartItems = [];
 addToCartButton.addEventListener('click', () => {
   cartItemValue.innerHTML = itemQuantity.innerHTML
+  cartItem.innerHTML = itemQuantity.innerHTML
+
+  cartTotal.innerHTML = itemPrice.innerHTML * cartItemValue.innerHTML;
+  cartTotal.innerHTML = Number(cartTotal.innerHTML).toFixed(2)
 
   cartItemValue.style.color = '#fff';
   cartItemValue.style.fontWeight = 'bold';
@@ -35,6 +54,23 @@ addToCartButton.addEventListener('click', () => {
     : cartItemQuantity.style.display = 'none';
 })
 
-cart.addEventListener('click', () => {
 
+const handleCartValue = () => {
+  cartCard.classList.toggle('active')
+  cartCardDiv.classList.toggle('active')
+  cartItemValue.innerHTML > 0
+    ? cartCardDiv.classList.toggle('active')
+    : emptyCartDiv.classList.toggle('active')
+}
+
+cartIcon.addEventListener('click', handleCartValue)
+
+let cartItemAmount = 0
+deleteItemFromCart.addEventListener('click', () => {
+  emptyCartDiv.classList.toggle('active')
+  cartCardDiv.classList.toggle('active')
 })
+
+
+
+
